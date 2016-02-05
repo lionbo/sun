98618be20aa49ac7d1ddc803e48091e38602ccb0
liuyongbo <liuyongbo@didichuxing.com>
2016-02-05 14:08:54 +0800

    deploy

---
+++ src/main/java/com/key4dream/sun/utils/Constants.java
@@ -0,0 +1,9 @@
+package com.key4dream.sun.utils;
+
+public final class Constants {
+
+    private final String GET_ACCESS_TOKEN = "/token";
+
+    private final String MSG_SEND = "/message/custom/send";
+
+}
